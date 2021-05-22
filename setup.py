0309af6acdea2cc6361737b42ddfa573bfd52ea0
Vishnunkumar <38393302+Vishnunkumar@users.noreply.github.com>
2021-05-22 13:35:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Vishnu N',                   # Type in your name
   author_email = 'vishnunkumar25@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Vishnunkumar/box_detection',   # Provide either the link to your github or to your website
-  download_url ='https://github.com/Vishnunkumar/box_detection/archive/refs/tags/v_02.tar.gz',    # I explain this later on
+  download_url ='https://github.com/Vishnunkumar/box_detection/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['Computer Vision', 'Machine learning', 'OpenCV'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
